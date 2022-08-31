f175304b6a77ded78b090b5ee6381762d5d5e777
websicle <89933280+webcicle@users.noreply.github.com>
2022-08-31 17:18:14 +0200

    Slug page finished

---
+++ src/pages/article/htmlComponents.ts
@@ -0,0 +1,25 @@
+import ArticleImage from '../../components/block-components/ArticleImage.astro';
+
+import HeadingOne from '../../components/block-components/ArticleHeadingOne.astro';
+import HeadingTwo from '../../components/block-components/ArticleHeadingTwo.astro';
+import HeadingThree from '../../components/block-components/ArticleHeadingThree.astro';
+import HeadingFour from '../../components/block-components/ArticleHeadingFour.astro';
+import Paragraph from '../../components/block-components/ArticleParagraph.astro';
+import BlockQuote from '../../components/block-components/ArticleBlockQuote.astro';
+import type { PortableTextComponents } from 'astro-portabletext';
+
+const myPortableTextComponents = {
+	type: {
+		image: ArticleImage,
+	},
+	block: {
+		h1: HeadingOne,
+		h2: HeadingTwo,
+		h3: HeadingThree,
+		h4: HeadingFour,
+		blockquote: BlockQuote,
+		normal: Paragraph,
+	},
+};
+
+export default myPortableTextComponents as Partial<PortableTextComponents>;
