0f3bdc29197c49f5c572713d4cd417c73135731e
websicle <89933280+webcicle@users.noreply.github.com>
2022-09-01 19:01:38 +0200

    Added React support and YouTube embedded component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [react()],
+});
