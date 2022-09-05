c55e450e0a06c584634bd159b5658da56818ca26
websicle <89933280+webcicle@users.noreply.github.com>
2022-09-05 15:31:12 +0200

    Added SSR for Vercel Edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/edge';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: vercel(),
 	integrations: [react()],
 });
